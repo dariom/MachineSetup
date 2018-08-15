06a3910b7a47bc7009e1486c455cecd105a78360
Dario Mratovich <dario.mratovich@readify.net>
2018-08-15 19:13:01 +0800

    Add PowerShell profile

---
+++ Windows/Configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,46 @@
+﻿# Ensure that Get-ChildItemColor is loaded
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+# Helper function to set location to the User Profile directory
+function cuserprofile { Set-Location ~ }
+Set-Alias ~ cuserprofile -Option AllScope
+
+# Helper function to show Unicode character
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+# Ensure posh-git is loaded
+Import-Module -Name posh-git
+
+# Start SshAgent if not already
+# Need this if you are using github as your remote git repository
+#if (! (ps | ? { $_.Name -eq 'ssh-agent'})) {
+#    Start-SshAgent
+#}
+
+# Ensure oh-my-posh is loaded
+Import-Module -Name oh-my-posh
+
+# Select the oh-my-posh theme (see https://github.com/JanDeDobbeleer/oh-my-posh)
+Set-Theme paradox
