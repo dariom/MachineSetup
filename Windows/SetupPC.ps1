6184615c048d82d4ce8bc75efc863d6d61ff6a2e
Dario Mratovich <dario.mratovich@readify.net>
2018-08-29 18:59:54 +0800

    Remove Microsoft Teams

--- Windows/SetupPC.ps1
+++ Windows/SetupPC.ps1
@@ -126,8 +126,6 @@ Write-Host "    Paint.net" -ForegroundColor Magenta
 choco install paint.net -y
 Write-Host "    WinMerge" -ForegroundColor Magenta
 choco install winmerge  -y
-Write-Host "    Microsoft Teams" -ForegroundColor Magenta
-choco install microsoft-teams -y
 Write-Host "    Remote Desktop Manager" -ForegroundColor Magenta
 choco install rdcman -y
 Write-Host "    Steam" -ForegroundColor Magenta
