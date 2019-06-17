39f7dacaa37bce718e9e204cff0ad8cc3f067a43
Dario Mratovich <dario.mratovich@readify.net>
2019-06-17 21:39:04 +0800

    Enable Cloud Clipboard

--- Windows/SetupPC.ps1
+++ Windows/SetupPC.ps1
@@ -277,6 +277,9 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" -N
 # Hide Cortana Circle
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type DWord -Value 0 -ErrorAction SilentlyContinue
 
+# Enable Cloud Clipboard
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Clipboard" -Name "EnableClipboardHistory" -Type DWord -Value 1 -ErrorAction SilentlyContinue
+
 # Enable Lock Screen Spotlight
 Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\CloudContent" -Name "ConfigureWindowsSpotlight" -Type DWord -Value 1 -ErrorAction SilentlyContinue
 
